cca411042c60a369f34749f36696a50742b9cc51
cospamental <cospa18@gmail.com>
2018-02-15 22:15:16 +0300

    added the main app view file

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,8 @@
 from flask import render_template, request, redirect, url_for, abort  
 from . import main  
-
-
-
-
-
+from .forms import BookingForm
+from ..models import Event, Role,User,Booking
+from .. import db
 
 @main.route('/')
 def index():
@@ -13,6 +11,25 @@ def index():
     '''
     title = 'Home - Welcome to Zuru Ke'
 
+    events = Event.query.all()
     
+    return render_template('index.html', title = title,events=events)
 
-    return render_template('index.html', title = title)
+@main.route('/event/<int:id>', methods=['GET','POST'])
+def events(id):
+
+    form = BookingForm()
+
+    if form.validate_on_submit():
+        event_name = form.event_name.data
+        email = form.email.data
+        phone_number = form.phone_number.data
+        new_booking = Booking(email=email,event_name=event_name,phone_number=phone_number)
+
+        new_booking.save_booking()
+        return redirect(url_for('main.index'))
+
+    event = Event.query.filter_by(id=id).first()
+    title = f'{event.event_name}'
+    
+    return render_template('event.html', title=title, event=event, booking_form=form)

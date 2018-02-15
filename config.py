941c7a9c97e293ba72a39f351bdd8d9607f457b1
cospamental <cospa18@gmail.com>
2018-02-15 22:09:35 +0300

    added the app config file

--- config.py
+++ config.py
@@ -1,42 +1,23 @@
 import os
 
+
 class Config:
-    '''
-    General configuration parent class
-    '''
-    SECRET_KEY = 'hskjksnicnhdhjdsuhgcbuye87'
-    SQLALCHEMY_DATABASE_URI = ''
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    
-   
+      SQLALCHEMY_TRACK_MODIFICATIONS = False
+      SECRET_KEY='jdhbfhhbfnbfdhfdhdhbhcbdbvd44555'
 
-    
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = ''
-
-    pass
-
+      @staticmethod
+      def init_app(app):
+            pass
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-    Config: The parent configuration class with General configuration settings
-    '''    
-    pass
-    
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    
+      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+      pass
 
-    DEBUG = True
+class DevConfig(Config):
+      SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pasco:12345@localhost/zuru'
+      DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig,
-'test':TestConfig
+'production':ProdConfig
 }
